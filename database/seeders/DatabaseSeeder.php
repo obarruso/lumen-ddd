5c7850256a5daac5a781c40e5f457ea89259c759
obarruso <oierbarruso@gmail.com>
2023-09-26 20:00:20 +0200

    User seed (admin and some users for testing)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(\Database\Seeders\UserSeeder::class);
         // $this->call('UsersTableSeeder');
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use App\Bitres\User\Application\UseCases\Commands\StoreUserCommand;
+use App\Bitres\User\Domain\Factories\UserFactory;
+use App\Bitres\User\Domain\Model\ValueObjects\Password;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        (new StoreUserCommand(UserFactory::new([
+            'uuid' => Str::uuid(),
+            'username' => 'admin',
+            'email' => 'admin@bitres.com',
+            'is_admin' => true,
+        ]), new Password('adminpolr', 'adminpolr')))->execute();
+        for( $i = 100; $i <= 120; $i++ ) {
+            $name = 'prueba' . $i;
+            (new StoreUserCommand(UserFactory::new([
+                'username' => $name,
+            ]), new Password($name, $name)))->execute();
+        }
+
+    }
+}
