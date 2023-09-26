6c41889c3d9bab044cebb194cd9391700c3144a0
Oier Barruso <obarruso@pop-os.localdomain>
2023-09-26 19:58:29 +0200

    Table migrations

---
+++ database/migrations/2023_09_22_135638_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->default(0);
+            $table->boolean('is_active')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
