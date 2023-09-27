5f6a774f9ae571f9498ff32f6ef718769500b597
obarruso <oierbarruso@gmail.com>
2023-09-27 23:07:10 +0200

    feat(user): Add new commands and add user policy

--- src/Bitres/User/Application/UseCases/Commands/StoreUserCommand.php
+++ src/Bitres/User/Application/UseCases/Commands/StoreUserCommand.php
@@ -5,11 +5,11 @@ namespace App\Bitres\User\Application\UseCases\Commands;
 use App\Bitres\User\Application\Exceptions\EmailAlreadyUsedException;
 use App\Bitres\User\Domain\Model\User;
 use App\Bitres\User\Domain\Model\ValueObjects\Password;
-use App\Bitres\User\Domain\Repositories\AvatarRepositoryInterface;
 use App\Bitres\User\Domain\Repositories\UserRepositoryInterface;
 use App\Bitres\User\Infrastructure\EloquentModels\UserEloquentModel;
 use App\Common\Domain\CommandInterface;
 use App\Bitres\User\Application\Exceptions\UuidAlreadyUsedException;
+use App\Bitres\User\Domain\Policies\UserPolicy;
 
 class StoreUserCommand implements CommandInterface
 {
@@ -24,6 +24,7 @@ class StoreUserCommand implements CommandInterface
 
     public function execute(): User
     {
+        authorize('store', UserPolicy::class);
         if (UserEloquentModel::query()->where('uuid', $this->user->uuid)->exists()) {
             throw new UuidAlreadyUsedException();
         }
