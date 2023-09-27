2328efccda3e88bc80c97ed7a41b2600882f8990
obarruso <oierbarruso@gmail.com>
2023-09-27 23:14:14 +0200

    WIP feat(user): Update user feature allready done, testing failed TODO: check w/ other client

---
+++ src/Bitres/User/Presentation/Http/Controllers/UpdateUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Bitres\User\Presentation\Http\Controllers;
+
+use App\Bitres\User\Application\Mappers\UserMapper;
+use App\Bitres\User\Application\UseCases\Commands\UpdateUserCommand;
+use App\Bitres\User\Domain\Model\ValueObjects\Password;
+use App\Common\Domain\Exceptions\UnauthorizedUserException;
+use App\Common\Presentation\Http\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateUserController extends Controller
+{
+  public function __invoke(Request $request): JsonResponse
+  {
+    try {
+      $params = $request->all();
+      $userData = UserMapper::fromRequest($request, $request->uuid);
+      $password = new Password($request->input('password'), $request->input('password_confirmation'));
+      (new UpdateUserCommand($userData, $password))->execute();
+    } catch (\DomainException $domainException) {
+      return response()->json(
+        [
+          'error' => [
+            'message' => $domainException->getMessage()
+          ]
+        ],
+        Response::HTTP_UNPROCESSABLE_ENTITY
+      );
+    } catch (UnauthorizedUserException $e) {
+      return response()->unauthorized([
+        'error' => [
+          'message' => $e->getMessage()
+        ]
+      ]);
+    }
+
+    return response()->noContent();
+  }
+}
