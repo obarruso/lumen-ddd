f740ff1596f90acedb20993b3b9d0d71da517bac
obarruso <oierbarruso@gmail.com>
2023-09-27 19:26:19 +0200

    Set to routes new endpoints

--- src/Bitres/Auth/Presentation/Http/routes.php
+++ src/Bitres/Auth/Presentation/Http/routes.php
@@ -1,10 +1,16 @@
 <?php
 
-use App\Bitres\Auth\Presentation\Http\Controllers\LoginController;
-use App\Bitres\User\Presentation\Http\Controllers\UserTestController;
+// use App\Bitres\Auth\Presentation\Http\Controllers\LoginController;
+// use App\Bitres\Auth\Presentation\Http\Controllers\LogoutController;
+// use App\Bitres\Auth\Presentation\Http\Controllers\RefreshTokenController;
+// use App\Bitres\Auth\Presentation\Http\Controllers\MeController;
 
 $router->group([
   'prefix' => 'auth',
+  'namespace' => 'App\Bitres\Auth\Presentation\Http\Controllers',
 ], function ($router) {
-    $router->post('/login', LoginController::class);
+  $router->post('login', LoginController::class);
+  $router->post('logout', LogoutController::class);
+  $router->post('refresh', RefreshTokenController::class);
+  $router->get('me', MeController::class);
 });
